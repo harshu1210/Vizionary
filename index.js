37245be8a17b1afb2289aa292f8ffcaa96170df6
harshu1210 <charshal40@gmail.com>
2024-05-17 07:15:48 +0530

    Graph-Href-Attributes

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 fetch('chartMenu.json')
     .then(response => response.json())
     .then(data => {
-        // Work with JSON data here
-        // console.log(data);
         let content = document.getElementById('chartContent')
-        // console.log(content)
         for (let i = 0; i < data.length; i++) {
+            let link = document.createElement("a")
+            link.setAttribute("href", data[i].href)
             let newDiv = document.createElement('div')
             newDiv.classList.add('chartMenuTile')
             let image = document.createElement('img')
@@ -15,9 +14,11 @@ fetch('chartMenu.json')
             let title = document.createElement('div')
             title.classList.add('chartMenuTileTitle')
             title.innerText = data[i].name
-            newDiv.appendChild(image)
-            newDiv.appendChild(title)
+            link.appendChild(image)
+            link.appendChild(title)
+            newDiv.appendChild(link)
             content.appendChild(newDiv)
+
         }
     })
     .catch(error => {
